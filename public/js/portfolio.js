e3355849107387ce398143bbe71bd9ca012f5fea
Timmhart <tim.hart@hva.nl>
2021-04-15 15:53:08 +0200

    changes made

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,7 +1,14 @@
-const header = document.querySelector("header");
-const sectionOne = document.querySelector("gradient");
+// Script voor het bewerken van de 
+// nav na de eerste section 
 
-const sectionOneOptions = {};
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".gradient");
+
+const faders = document.querySelectorAll('.fadeIn');
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
 
 const sectionOneObserver = new IntersectionObserver(function(
   entries, 
@@ -10,9 +17,79 @@ const sectionOneObserver = new IntersectionObserver(function(
     entries.forEach(entry => {
       if(!entry.isIntersecting) {
         header.classList.add("nav-scrolled");
+      } else {
+        header.classList.remove("nav-scrolled");
       }
     });
 }, 
 sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {};
+
+const appearOnScroll = new IntersectionObserver
+(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+}, 
+options);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+// Javascript voor het contactformulier 
+
+const contactForm = document.querySelector('.contact-form');
+
+let name = document.getElementById('name'),
+    email = document.getElementById('email'),
+    subject = document.getElementById('subject'),
+    message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value
+  }
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = function(){
+    console.log(xhr.responseText);
+    if(xhr.responseText == 'succes'){
+      alert('Email sent');
+      name.value = '';
+      email.value = '';
+      subject.value = '';
+      message.value = '';
+    }else{
+      alert('Something went wrong!')
+    }
+  }
+
+  xhr.send(JSON.stringify(formData))
+})
+
+
+const names = ['Developer', 'Front-end Developer', 'UX Designer', 'Designer', 'Artist', 'Friend'];
+let i = 0;
+
+while(i < names.length){
+  console.log(names[i]);
+  i++;
+}
--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 const port = 3000;
 const express = require('express');
 const app = express();
+const nodemailer = require("nodemailer");
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('index')
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get('/test', (req, res) => {
+    res.sendFile(__dirname + '/public/test.html')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'timmhart1@gmail.com',
+            pass: 'Pieter16'
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'timhart13@live.nl',
+        subject: `Subject from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            console.log(error);
+            res.send('error');
+        }else {
+            console.log('Email sent: ' + info.response);
+            res.send('succes')
+        }
+    })
 });
 
 app.listen(port, () => {
